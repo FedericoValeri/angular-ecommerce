46d56b4981cf22493ec240489428ac6a17c9ab9c
Federico <federico.valeri@unicam.it>
2023-01-26 13:13:52 +0100

    Get products request

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { Product } from '../common/product';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
+  private baseURL = 'http://localhost:8080/api/products';
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {}
+
+  getProductList(): Observable<Product[]> {
+    return this.httpClient
+      .get<GetResponse>(this.baseURL)
+      .pipe(map((response) => response._embedded.products));
+  }
+}
+
+interface GetResponse {
+  _embedded: {
+    products: Product[];
+  };
 }
